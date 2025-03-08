78dd9b21493420eb894db07ff6e69f8fc242fe01
Shunpei <pirikinoko@gmail.com>
2025-03-08 13:39:07 +0900

    パラメータのコピーを生成できるように

--- Assets/ScriptableObjects/Parameter/ParameterAsset.cs
+++ Assets/ScriptableObjects/Parameter/ParameterAsset.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ParameterAsset", menuName = "ScriptableObjects/ParameterAsset")]
@@ -17,4 +18,26 @@ public class Parameter
     public int Power;
     public List<Skill> Skills;
     public Sprite IconSprite;
+
+    // コピーコンストラクタ
+    public Parameter(Parameter original)
+    {
+        EntityType = original.EntityType;
+        Name = original.Name;
+        HitPoint = original.HitPoint;
+        ManaPoint = original.ManaPoint;
+        Power = original.Power;
+
+        // スキルのディープコピー
+        Skills = original.Skills.Select(skill => new Skill(skill.Name, skill.EffectId)).ToList();
+
+        IconSprite = original.IconSprite;
+    }
+
+    // クローンメソッド
+    public Parameter Clone()
+    {
+        return new Parameter(this);
+    }
 }
+
